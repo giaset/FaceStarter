9be7e5cc5b44070a78c8a8b60af0ecd63e057ec2
Jack Rogers <jack@clarifai.com>
2016-05-04 23:39:27 -0400

    Initial Commit

---
+++ ME/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ME
+//
+//  Created by Jack Rogers on 5/4/16.
+//  Copyright © 2016 Jack Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
